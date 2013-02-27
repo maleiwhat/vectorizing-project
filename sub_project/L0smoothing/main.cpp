fc957bc242e4eb200c71560aa9ae920e82b7524c
damody <t1238142000@gmail.com>
2013-02-28 07:17:39 +0800

    add L0smoothing

---
+++ sub_project/L0smoothing/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <auto_link_fftw.hpp>
+#include <auto_link_opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <fftw3.h>
+
+
+int main()
+{
+
+}
